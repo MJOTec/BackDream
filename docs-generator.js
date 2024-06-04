d8298c150730b0f1519c37acbf9999901de1bf61
MJOTec <105307387+MJOTec@users.noreply.github.com>
2024-06-04 14:30:59 -0600

    cambio ruta documentacion a azure

--- docs-generator.js
+++ docs-generator.js
@@ -1,6 +1,9 @@
 var swaggerAutogen = require('swagger-autogen')({openapi: '3.0.0'});
 var outputFile = "./swagger.json";
 var endpointsFiles = ["./api.js"];
-var config = {};
+var config = {
+    host: "apidream.azurewebsites.net",
+    schemes: ['https']
+};
 
 swaggerAutogen(outputFile, endpointsFiles, config);
