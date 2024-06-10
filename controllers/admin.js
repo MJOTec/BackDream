045fd66827a5709df53c9be0ea8f184c010cfb55
MichelleBG18 <A00833416@itesm.mx>
2024-06-09 23:55:44 -0600

    Salas y Dispositivos por dia, mes y año

--- controllers/admin.js
+++ controllers/admin.js
@@ -21,4 +21,64 @@ module.exports = {
                 .json({ message: `Error al obtener la reserva. Err: ${err}` });
         }
     },
+    getAdminDispositivosReservadosMes: async (req, res) => {
+        try {
+            DispositivosReservadosMes = await AdminServices.getAdminDispositivosReservadosMes()
+            return res.status(200).json(DispositivosReservadosMes)
+        } catch (err) {
+            return res
+                .status(500)
+                .json({ message: `Error al obtener los dispositivos. Err: ${err}` });
+        }
+    },
+    getAdminDispositivosReservadosDia: async (req, res) => {
+        try {
+            DispositivosReservadosDia = await AdminServices.getAdminDispositivosReservadosDia()
+            return res.status(200).json(DispositivosReservadosDia)
+        } catch (err) {
+            return res
+                .status(500)
+                .json({ message: `Error al obtener los dispositivos. Err: ${err}` });
+        }
+    },
+    getAdminDispositivosReservadosAno: async (req, res) => {
+        try {
+            DispositivosReservadosAno = await AdminServices.getAdminDispositivosReservadosAno()
+            return res.status(200).json(DispositivosReservadosAno)
+        } catch (err) {
+            return res
+                .status(500)
+                .json({ message: `Error al obtener los dispositivos. Err: ${err}` });
+        }
+    },
+    getAdminSalasReservadasMes: async (req, res) => {
+        try {
+            SalasReservadasMes = await AdminServices.getAdminSalasReservadasMes()
+            return res.status(200).json(SalasReservadasMes)
+        } catch (err) {
+            return res
+                .status(500)
+                .json({ message: `Error al obtener las salas. Err: ${err}` });
+        }
+    },
+    getAdminSalasReservadasDia: async (req, res) => {
+        try {
+            SalasReservadasDia = await AdminServices.getAdminSalasReservadasDia()
+            return res.status(200).json(SalasReservadasDia)
+        } catch (err) {
+            return res
+                .status(500)
+                .json({ message: `Error al obtener las salas. Err: ${err}` });
+        }
+    },
+    getAdminSalasReservadasAno: async (req, res) => {
+        try {
+            SalasReservadasAno = await AdminServices.getAdminSalasReservadasAno()
+            return res.status(200).json(SalasReservadasAno)
+        } catch (err) {
+            return res
+                .status(500)
+                .json({ message: `Error al obtener las salas. Err: ${err}` });
+        }
+    },
 };
