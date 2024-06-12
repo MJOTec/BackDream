9e4e0a693785969ba19d7fe0832b2fcb99691e69
EduardoGS <88677782+EduarddoGS@users.noreply.github.com>
2024-06-12 02:24:22 -0600

    cambio nombre sala en evento

--- services/eventos.js
+++ services/eventos.js
@@ -3,8 +3,9 @@ const dbService = require('../config/db.js');
 module.exports = {
     getEventosQuery: async () => {
         const pool = await dbService.poolPromise;
-        const sql = `SELECT *
-                    FROM Evento`;
+        const sql = `SELECT Evento.*, Sala.nombre AS nombre_sala
+        FROM Evento
+        JOIN Sala ON Evento.id_sala = Sala.id_sala;`;
         const result = await pool.request().query(sql);
         console.log("", result);
         return result.recordset;
